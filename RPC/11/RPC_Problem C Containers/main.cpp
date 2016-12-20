9e7e6d0cd976e9b45db81f85491199d2b866e017
Melvin Alonso <melvin.aep@hotmail.com>
2016-12-19 19:16:09 -0500

    Update main.cpp

--- RPC/11/RPC_Problem C Containers/main.cpp
+++ RPC/11/RPC_Problem C Containers/main.cpp
@@ -4,28 +4,20 @@ using namespace std;
 typedef pair<int, vector<int>> pii;
 
 vector<int> initial(8), final(8);
-// This shit is awesome, first time I use one xD
 priority_queue<pii, vector<pii>, greater<pii>> pq;
-// Cool, to mark boards as visited
 set<vector<int>> visitedSet;
 
-// #ILoveDijkstra
 int dijkstra() {
-    // Push first board to priority queue
     pq.push({0, initial});
+    vector<int> curr;
+    int cost;
     while (true) {
-        int cost;
-        vector<int> curr;
-        // Tie is from tuple. Cool trick btw
-        tie(cost, curr) = pq.top();
-        pq.pop();
-
-        // If I have already stayed in this board, just skip it
+        tie(cost, curr) = pq.top(), pq.pop();
+        
         if (visitedSet.find(curr) != visitedSet.end()) continue;
-        // In case we reach the goal... (We can compare vectors just by using ==)
+        
         if (curr == final) return cost;
 
-        // Mark as visited
         visitedSet.insert(curr);
 
         // Swap adjacent containers in the same row
@@ -33,7 +25,6 @@ int dijkstra() {
             if (i % 4 != 3) {
                 swap(curr[i], curr[i + 1]);
                 if (visitedSet.find(curr) == visitedSet.end()) {
-                    // Push with the new cost, according the weight of the containers swapped.
                     pq.push({cost + curr[i] + curr[i + 1], curr});
                 }
                 swap(curr[i], curr[i + 1]);
@@ -44,12 +35,10 @@ int dijkstra() {
         for (int i = 0; i < 4; i++) {
             swap(curr[i], curr[i + 4]);
             if (visitedSet.find(curr) == visitedSet.end()) {
-                // Push with the new cost, according the weight of the containers swapped.
                 pq.push({cost + curr[i] + curr[i + 4], curr});
             }
             swap(curr[i], curr[i + 4]);
         }
-
     }
 }
 
@@ -58,16 +47,14 @@ int main() {
     cin.tie(0);
 
     while (cin >> initial[0]) {
-        // Read the boards as a vector.
+
         for (int i = 1; i < 8; i++) cin >> initial[i];
         for (int i = 0; i < 8; i++) cin >> final[i];
 
-        // Magic part
         cout << dijkstra() << "\n";
 
-        // Clean the priority queue
+
         while (!pq.empty()) pq.pop();
-        // Clean the set
         visitedSet.clear();
     }
 
